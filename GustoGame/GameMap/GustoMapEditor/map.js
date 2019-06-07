93dce03417725b42f9e484cd9c5089bf8e0de058
Garritt Moede <garritt.moede@gmail.com>
2019-06-06 23:32:38 -0500

    game map now has localized regions

--- GustoGame/GameMap/GustoMapEditor/map.js
+++ GustoGame/GameMap/GustoMapEditor/map.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	var tileDict = {};
 	var selectedColor = "#5773B2";
 	var selectedTerrainKey = "o1";
+	var selectedRegionName = "GustoMap";
+	var selectedNickName = "GG";
 
 	$('#init').click(function() {
 		var width = parseInt($('#width').val());
@@ -16,7 +18,25 @@ $(document).ready(function(){
 
 	$('#exportjson').click(function(){
 		var fname = $('#filename').val(); 
-		download(JSON.stringify(tileDict), fname, "application/json")
+		download(JSON.stringify(tileDict), fname, "application/json");
+		download("var mapdata = " + JSON.stringify(tileDict), "mapdata.js", "text/javascript");
+	});
+
+	$('#loadjson').click(function(){
+		var loadedMap = JSON.parse(JSON.stringify(mapdata));
+		LoadMap(mapdata, parseInt($('#width').val()), parseInt($('#height').val()), parseInt($('#multX').val()), parseInt($('#multY').val()), parseInt($('#tileSize').val()))
+	});
+
+	$('#newRegion').click(function(){
+		$('.selectRegion').prop('checked', false);
+		$('#regionList').append('<li><input type="radio" class="selectRegion" checked><input type="text" value="regionName" class="region"><input type="text" value="nickName" class="nickName"></li>')
+	});
+
+	$('#regionList').on('click', '.selectRegion', function(e){
+		$('.selectRegion').prop('checked', false);
+		$(this).prop('checked', true);
+		selectedRegionName = $(this).siblings('.region').val();
+		selectedNickName = $(this).siblings('.nickName').val();
 	});
 
 	$('.terrain').click(function() {
@@ -27,12 +47,56 @@ $(document).ready(function(){
 	});
 
 	$('#map').on('mousemove', '.tile', function(e){
+		
+		var id = $(this).attr('id');
+
 		if (e.buttons === 1) {
 	    	$(this).css({'background-color': selectedColor});
-	    	var id = $(this).attr('id');
-	    	tileDict[id] = selectedTerrainKey;
+	    	var entry = {
+	    		terrainPiece:selectedTerrainKey,
+	    		regionName:selectedRegionName
+	    	};
+	    	tileDict[id] = entry;
+
+	    	$(this).text(selectedNickName);
+	    	$(this).css("fontSize", 10);
 		}
 	});
+
+	function LoadMap(mapdata, width, height, multX, multY, tileSize) {
+
+		tileDict = mapdata;
+		$('#map').empty();
+
+		var cols = Math.floor((width / tileSize) * multX);
+		var rows = Math.floor((height / tileSize) * multY);
+
+		var displayTileSizeX = 16;
+		var displayTileSizeY = 16;
+
+		document.getElementById('map').style.setProperty('--ncols', cols.toString());
+		document.getElementById('map').style.setProperty('--nrows', rows.toString());
+		document.getElementById('map').style.setProperty('--tileX', displayTileSizeX.toString() + "px");
+		document.getElementById('map').style.setProperty('--tileY', displayTileSizeY.toString() + "px");
+
+		var index = 0;
+		for (var i = 0; i < rows; i++) {
+			for (var j = 0; j < cols; j++) {
+				$('#map').append('<div class="tile" id="'+ index + '" style="width:' + displayTileSizeX + 'px;height:' + displayTileSizeY + 'px;"></div>');
+				var entry = tileDict[index];
+				$('#' + index).css({'background-color': $('#' + entry['terrainPiece']).css("background-color")})
+				$('#' + index).text(entry['regionName'].substring(0, 2))
+		    	$('#' + index).css("fontSize", 10);
+				index++;
+			}
+		}
+
+		$('#nTiles').text((cols * rows));
+		$('#cols').text(cols);
+		$('#rows').text(rows);
+
+	}
+
 
 	function BuildMap(width, height, multX, multY, tileSize) {
 		
@@ -54,7 +118,11 @@ $(document).ready(function(){
 		for (var i = 0; i < rows; i++) {
 			for (var j = 0; j < cols; j++) {
 				$('#map').append('<div class="tile" id="'+ index + '" style="width:' + displayTileSizeX + 'px;height:' + displayTileSizeY + 'px;"></div>');
-				tileDict[index] = selectedTerrainKey;
+		    	var entry = {
+		    		terrainPiece:selectedTerrainKey,
+		    		regionName:selectedRegionName
+		    	};
+	    		tileDict[index] = entry;
 				index++;
 			}
 		}
