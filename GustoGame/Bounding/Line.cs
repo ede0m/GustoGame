66446e1834904b0a8e32f7b1091f17443b16e676
Garritt Moede <garritt.moede@gmail.com>
2019-07-09 22:20:27 -0500

    polygon texture extraction

---
+++ GustoGame/Bounding/Line.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gusto.Bounds
+{
+    public class Line
+    {
+        public Vector2 Start;
+        public Vector2 End;
+    }
+}
--- GustoGame/GameGusto.cs
+++ GustoGame/GameGusto.cs
@@ -103,41 +103,41 @@ namespace Gusto
 
             // PREPROCESSING
             Texture2D textureBaseShip = Content.Load<Texture2D>("BaseShip");
-            LoadDynamicBoundingBoxPerFrame(8, 1, textureBaseShip, "baseShip", 0.6f);
+            LoadDynamicBoundingBoxPerFrame(true, 8, 1, textureBaseShip, "baseShip", 0.6f);
             Texture2D texturePlayerPirate = Content.Load<Texture2D>("Pirate1-combat");
-            LoadDynamicBoundingBoxPerFrame(4, 11, texturePlayerPirate, "playerPirate", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 4, 11, texturePlayerPirate, "playerPirate", 1.0f);
             Texture2D textureBaseTribal = Content.Load<Texture2D>("Tribal1");
-            LoadDynamicBoundingBoxPerFrame(4, 12, textureBaseTribal, "baseTribal", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 4, 12, textureBaseTribal, "baseTribal", 1.0f);
             Texture2D textureTribalTokens = Content.Load<Texture2D>("TribalTokens");
-            LoadDynamicBoundingBoxPerFrame(1, 1, textureTribalTokens, "tribalTokens", 0.5f);
+            LoadDynamicBoundingBoxPerFrame(false, 1, 1, textureTribalTokens, "tribalTokens", 0.5f);
             Texture2D textureBaseSword = Content.Load<Texture2D>("BaseSword");
-            LoadDynamicBoundingBoxPerFrame(4, 3, textureBaseSword, "baseSword", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 4, 3, textureBaseSword, "baseSword", 1.0f);
             Texture2D texturePistol = Content.Load<Texture2D>("pistol");
-            LoadDynamicBoundingBoxPerFrame(4, 3, texturePistol, "pistol", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 4, 3, texturePistol, "pistol", 1.0f);
             Texture2D textureShortSword = Content.Load<Texture2D>("ShortSword");
-            LoadDynamicBoundingBoxPerFrame(4, 3, textureShortSword, "shortSword", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 4, 3, textureShortSword, "shortSword", 1.0f);
             Texture2D textureBaseSail = Content.Load<Texture2D>("DecomposedBaseSail");
-            LoadDynamicBoundingBoxPerFrame(8, 3, textureBaseSail, "baseSail", 0.6f);
+            LoadDynamicBoundingBoxPerFrame(false, 8, 3, textureBaseSail, "baseSail", 0.6f);
             Texture2D textureTower = Content.Load<Texture2D>("tower");
-            LoadDynamicBoundingBoxPerFrame(1, 1, textureTower, "tower", 0.5f);
+            LoadDynamicBoundingBoxPerFrame(false, 1, 1, textureTower, "tower", 0.5f);
             Texture2D textureCannonBall = Content.Load<Texture2D>("CannonBall");
-            LoadDynamicBoundingBoxPerFrame(1, 2, textureCannonBall, "baseCannonBall", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 1, 2, textureCannonBall, "baseCannonBall", 1.0f);
             Texture2D textureCannonBallItem = Content.Load<Texture2D>("CannonBall");
-            LoadDynamicBoundingBoxPerFrame(1, 2, textureCannonBallItem, "cannonBallItem", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 1, 2, textureCannonBallItem, "cannonBallItem", 1.0f);
             Texture2D texturePistolShot = Content.Load<Texture2D>("PistolShot");
-            LoadDynamicBoundingBoxPerFrame(1, 2, texturePistolShot, "pistolShot", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 1, 2, texturePistolShot, "pistolShot", 1.0f);
             Texture2D texturePistolShotItem = Content.Load<Texture2D>("PistolShot");
-            LoadDynamicBoundingBoxPerFrame(1, 2, texturePistolShotItem, "pistolShotItem", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 1, 2, texturePistolShotItem, "pistolShotItem", 1.0f);
             Texture2D textureBaseCannon = Content.Load<Texture2D>("BaseCannon");
-            LoadDynamicBoundingBoxPerFrame(8, 1, textureBaseCannon, "baseCannon", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 8, 1, textureBaseCannon, "baseCannon", 1.0f);
             
             // Tile Pieces
             Texture2D textureOcean1 = Content.Load<Texture2D>("Ocean1");
-            LoadDynamicBoundingBoxPerFrame(1, 4, textureOcean1, "oceanTile", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 1, 4, textureOcean1, "oceanTile", 1.0f);
             Texture2D textureLand1 = Content.Load<Texture2D>("Land1");
-            LoadDynamicBoundingBoxPerFrame(1, 4, textureLand1, "landTile", 1.0f);
+            LoadDynamicBoundingBoxPerFrame(false, 1, 4, textureLand1, "landTile", 1.0f);
             Texture2D textureTree1 = Content.Load<Texture2D>("Tree1");
-            LoadDynamicBoundingBoxPerFrame(2, 6, textureTree1, "tree1", 0.4f);
+            LoadDynamicBoundingBoxPerFrame(false, 2, 6, textureTree1, "tree1", 0.4f);
 
             // Game Map
             map.SetGameMap(Content, GraphicsDevice);
@@ -186,7 +186,7 @@ namespace Gusto
         }
 
         // creates dynamic bounding boxes for each sprite frame
-        private void LoadDynamicBoundingBoxPerFrame(int rows, int columns, Texture2D source, string key, float scale)
+        private void LoadDynamicBoundingBoxPerFrame(bool polygon, int rows, int columns, Texture2D source, string key, float scale)
         {
             int width = source.Width / columns;
             int height = source.Height / rows;
@@ -201,6 +201,11 @@ namespace Gusto
                     Color[] data = new Color[sourceRectangle.Width * sourceRectangle.Height];
                     source.GetData(0, sourceRectangle, data, 0, data.Length);
                     cropTexture.SetData(data);
+
+                    List <Line> bp = null;
+                    if (polygon)
+                        bp = CalculateTextureBoundingBox.CropTextureToPolygon(cropTexture);
+
                     // store the BB texture 
                     Rectangle bb = CalculateTextureBoundingBox.GetSmallestRectangleFromTexture(cropTexture, scale);
                     BoundingBoxTextures.DynamicBoundingBoxTextures[key].Add(i.ToString() + j.ToString(), bb);
